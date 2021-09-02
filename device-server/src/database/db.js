3a88ea5a1c36d95dfc6e9bcf862d295c748651d8
unknown <vaskarsarma@gmail.com>
2021-09-02 21:34:58 +0400

    Read DB Connection String from ENV variables

--- device-server/src/database/db.js
+++ device-server/src/database/db.js
@@ -1,3 +1,5 @@
+const config = require('../config/config');
+
 module.exports = {
-  db: 'mongodb+srv://vaskartest:Q8IDHIa7KzMak1Wi@testvaskar.u4d65.mongodb.net/DeviceList?retryWrites=true&w=majority',
+  db: config.dbconstring
 };
